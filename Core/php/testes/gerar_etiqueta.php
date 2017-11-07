d1061287b8a028da8dd3dd53f5e6661a990cec6f
Dimitri Leandro <dimitri.leandro@gmail.com>
2017-11-07 15:37:36 -0200

    Gerando e Imprimindo Etiquetas

---
+++ Core/php/testes/gerar_etiqueta.php
@@ -0,0 +1,16 @@
+<?php
+	require_once('../model/paciente.Class.php');
+    $paciente = new Paciente();
+    //obtendo os dados do paciente
+    $paciente -> selecionar_paciente($_GET['cd_paciente']);
+?>
+<html>
+<body>
+	<div id="div_pdf" style="font-size: 250%;">
+		<p><?php echo $paciente ->  get_nm_paciente(); ?></p>
+		<p><?php echo $paciente ->  get_dt_nascimento(); ?></p>
+		<p><?php echo date("Y-m-d").' '.date("H:i:s"); ?></p>
+	</div>
+	
+</body>
+</html>
