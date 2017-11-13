125ad305f074df8c7c32db81c1b3fc6f83485043
DimitriLeandro <dimitri.leandro@gmail.com>
2017-11-13 00:24:07 -0200

    Mudanças na classe Triagem. Bugs corrigidos.

--- Core/cadastrar_triagem.php
+++ Core/cadastrar_triagem.php
@@ -78,17 +78,17 @@ require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
 
             //cadastrando 
             $ok = $triagem -> cadastrar_triagem();
-            
-            $mensagem = 'Triagem cadastrada com sucesso';
-            if($ok == 0)
-            {
-              $mensagem = 'Erro ao cadastrar triagem';
-            }
-
             unset($triagem);
 
-            ?> <script> alert("<?php echo ''.$mensagem; ?>"); </script> <?php
-            header('location: pesquisar_paciente.php');
+            //$mensagem = 'Triagem cadastrada com sucesso';
+            if($ok == 0)
+            {
+              ?> <script> alert('Erro ao registrar triagem. Verifique os dados inseridos.'); </script> <?php
+            }
+            else
+            {
+              header('location: pesquisar_paciente.php');
+            }
         }
         else
         {
