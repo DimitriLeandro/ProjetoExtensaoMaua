f4df326a80e6509071542e518cfe241a9c7feb99
DimitriLeandro <dimitri.leandro@gmail.com>
2017-11-17 19:24:22 -0200

    Melhorias na pesquisa de pacientes
    Tudo agora está sendo feito com ajax, sem método POST, somente GET.

--- Core/pesquisar_paciente.php
+++ Core/pesquisar_paciente.php
@@ -14,69 +14,53 @@ require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
 
 
 <?php
-    require_once 'users/init.php';
-    $db = DB::getInstance();
-    if (!securePage($_SERVER['PHP_SELF'])){die();} 
-?>
-
-<?php
-    if(isset($_POST['btn_cadastrar']))
-    {
-        header('location: cadastrar_paciente.php');
-    }
+	require_once 'users/init.php';
+	$db = DB::getInstance();
+	if (!securePage($_SERVER['PHP_SELF'])){die();} 
 ?>
 
 <html>
 <head>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
-  <script src="users/js/jquery.js"></script>
-  <title>Pesquisar Paciente</title>
-  <meta charset="utf-8" />
-  <link href="css/formulario.css" rel="stylesheet">
+  	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+  	<script src="users/js/jquery.js"></script>
+  	<title>Pesquisar Paciente</title>
+  	<meta charset="utf-8" />
+  	<link href="css/formulario.css" rel="stylesheet">
 </head>
-<script>
-    function imprimir(html_frame)
-    {
-        $("#"+html_frame+"").show();
-        window.frames[html_frame].focus();
-        window.frames[html_frame].print();
-        $("#"+html_frame+"").hide();
-    }
-</script>
 <body>
-
   <div>
+	<form method="post" class="form-style">
+		<h1>PESQUISAR PACIENTE</h1>
+	<fieldset>
+		<label for="ncns" class="margem">Número CNS</label>
+		<input type="number" name="cd_cns_paciente" id="cd_cns_paciente" /><br />
+		
+		<label for="nomep" class="margem">Nome</label>
+		<input type="text" name="nm_paciente" id="nm_paciente" /><br />
+	</fieldset><br />
 
-    <form method="post" class="form-style">
-        <h1>PESQUISAR PACIENTE</h1>
-    <fieldset>
-        <label for="ncns" class="margem">Número CNS</label>
-        <input type="number" name="cd_cns_paciente" id="ncns" /><br />
-        
-        <label for="nomep" class="margem">Nome</label>
-        <input type="text" name="nm_paciente" id="nomep" /><br />
-    </fieldset><br />
-
-    <p>
-      <input type="submit" name='btn_pesquisar' value="Pesquisar" />
-      <input type="submit" name='btn_cadastrar' value="Cadastrar Novo Paciente" />
-    </p>
-    <br />
-    <div id="div_1">
-      <?php
-      //essa div fica invisivel até que o botão pesquisar seja setado, nesse caso, o arquivo div_pesquisar_paciente.php se encarrega de exibir o resultado da pesquisa.
-        if(isset($_POST['btn_pesquisar']))
-        {
-            if($_POST['cd_cns_paciente'] != '' || $_POST['nm_paciente'] != '')
-            {
-                $cd_cns_paciente = $_POST['cd_cns_paciente'];
-                $nm_paciente = $_POST['nm_paciente'];
-                require_once('php/div_pesquisar_paciente.php');
-            }
-        }
-      ?>
-    </div>
-    </form>
+	<p>
+		<button type="button" class="botao" id="btn_pesquisar">Pesquisar</button>
+		<button type="button" class="botao" id="btn_cadastrar">Cadastrar Novo Paciente</button>
+	</p>
+	<br />
+	<div id="div_resultados">
+	</div>
+	</form>
   </div>
+  	<script>
+			//funçao de pesquisar
+			$("#btn_pesquisar").on("click", function(){
+				var txt_nome = $("#nm_paciente").val();
+				var txt_cns = $("#cd_cns_paciente").val();
+				var redirect = "php/div_pesquisar_paciente.php?nm_paciente="+txt_nome+"&cd_cns_paciente="+txt_cns;
+				$("#div_resultados").load(redirect+"");
+			});
+
+			$("#btn_cadastrar").on("click", function(){
+				window.location = "cadastrar_paciente.php";
+			});
+
+	</script>
 </body>
 </html>
