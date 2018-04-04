baf20e4af6d6a898638daf0fb5efca8d0c283842
DimitriLeandro <dimitri.leandro@gmail.com>
2018-04-04 15:01:17 -0300

    Pré-Merge

--- Core/php/testes/teste_classe_ubs.php
+++ Core/php/testes/teste_classe_ubs.php
@@ -2,13 +2,17 @@
     <?php
     require_once '../classes/ubs.Class.php';
 
-    $obj_ubs = new Ubs();
+    if (isset($_GET['cd_cep'])) {
+        $obj_ubs = new Ubs();
 
-    $obj_ubs->pesquisarPorCep($_GET['cd_cep']);
-    echo "<br/>".$obj_ubs->getCdUbs();
-    echo "<br/>".$obj_ubs->getCdCep();
-    echo "<br/>".$obj_ubs->getNmLogradouro();
-    echo "<br/>".$obj_ubs->getNmBairro();
-    echo "<br/>".$obj_ubs->getNmUbs();
+        $obj_ubs->pesquisarPorCep($_GET['cd_cep']);
+        echo "<br/>" . $obj_ubs->getCdUbs();
+        echo "<br/>" . $obj_ubs->getCdCep();
+        echo "<br/>" . $obj_ubs->getNmLogradouro();
+        echo "<br/>" . $obj_ubs->getNmBairro();
+        echo "<br/>" . $obj_ubs->getNmUbs();
+    } else {
+        header("location: ../../index.php");
+    }
     ?> 
 </pre>
