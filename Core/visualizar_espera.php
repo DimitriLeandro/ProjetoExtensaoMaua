ee119bcb24c67d35a9facd2e14864437bad0173b
DimitriLeandro <dimitri.leandro@gmail.com>
2018-06-04 15:26:53 -0300

    Arrumando bugs para o Bot

--- Core/visualizar_espera.php
+++ Core/visualizar_espera.php
@@ -44,7 +44,62 @@ if (isset($_GET['remover']) && $_GET['remover'] > 0) {
 	<?php require_once 'php/div_header.php'; ?>
         <form class="form-style">
             <h1>LISTA DE ESPERA</h1><br/>
-            <div id="div_lista_espera"></div>
+            <div id="div_lista_espera">
+                <?php
+                    //instanciando um objeto de Espera para pegar a lista completa
+                    require_once('php/classes/espera.Class.php');
+                    $obj_espera = new Espera();
+
+                    $lista = $obj_espera->selecionarListaCompleta();
+
+                    foreach ($lista as $row) {
+                        $redirect_nova_triagem = "cadastrar_triagem.php?cd_paciente=" . $row['cd_paciente'];
+                        $redirect_historico = "pesquisar_triagem.php?cd_paciente=" . $row['cd_paciente'];
+                        ?>
+                        <fieldset style = "border: solid 1px; padding: 10px;">
+                            <table>
+                            <tr>
+                                <th>
+                                <label class="margem">Nome: <?php echo $row['nm_paciente']; ?></label>
+                                </th>
+                                <th>
+                                <label class="margem">Sexo: <?php echo $row['ic_sexo']; ?></label>
+                                </th>
+                                <th>
+                                <label class="margem">Raça: <?php echo $row['ic_raca']; ?></label>
+                                </th>
+                                <th>
+                                <label class="margem">Idade: <?php echo $row['vl_idade']; ?></label>
+                                </th>
+                            </tr>
+                            <tr>
+                                <th>
+                                <label class="margem">Data: <?php echo $row['dt_registro']; ?></label>
+                                </th>
+                                <th>
+                                <label class="margem">Hora: <?php echo $row['hr_registro']; ?></label>
+                                </th>
+                            </tr>
+                            </table>
+                            <p>
+                            <?php
+                            //Os botões "Nova triagem" e "Histŕico de Triagens" só deve aparecer se o usuário logado for um enfermeiro
+                            require_once 'php/classes/usuario.Class.php';
+                            $obj_usuario = new Usuario();
+                            if ($obj_usuario->getPermission() == "Enfermeiro" || $obj_usuario->getPermission() == "Administrator") {
+                            ?>
+                            <button type="button" id="<?php echo $redirect_nova_triagem; ?>" class="botao" onclick="window.location.href = '<?php echo $redirect_nova_triagem; ?>';">Nova Triagem</button>
+                            <button type="button" class="botao" onclick="window.location.href = '<?php echo $redirect_historico; ?>';">Histórico de Triagens</button>
+                            <?php
+                            }
+                            ?>
+                            <button type="button" class="botao" onclick="window.location.href = 'visualizar_espera.php?remover=<?php echo $row['cd_espera']; ?>';">Retirar Paciente da Lista</button>
+                            </p>
+                        </fieldset><br/>
+                        <?php
+                    }
+                    ?>
+            </div>
 	    <?php
 	    //Os botões "Nova triagem" e "Histŕico de Triagens" só deve aparecer se o usuário logado for um enfermeiro
 	    require_once 'php/classes/usuario.Class.php';
@@ -61,7 +116,6 @@ if (isset($_GET['remover']) && $_GET['remover'] > 0) {
         <script>
             //FUNÇÃO QUE FICA RESPONSÁVEL POR RECARREGAR A LISTA A CADA 5 SEGUNDOS
             $(document).ready(function () {
-                recarregar_lista();
                 setInterval(function () {
                     recarregar_lista()
                 }, 5000);
